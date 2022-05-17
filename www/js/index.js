44275b093479fb5ed16c856431560af2ab8d9c2c
Vinny Teixeira <87780451+vinnyteixeira@users.noreply.github.com>
2022-05-17 20:29:12 -0300

    cordova com sql

--- www/js/index.js
+++ www/js/index.js
@@ -1,28 +1,34 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 
-// Wait for the deviceready event before using any of Cordova's device APIs.
-// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
 document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
+
+    
+    let configDb = {
+        name: 'meusqlite',
+        location: 'default',
+        androidDatabaseProvider: 'system'
+
+    }
+    let db = window.sqlitePlugin.openDatabase(configDb);
+
+    db.transaction(function(tx) {
+        tx.executeSql('CREATE TABLE IF NOT EXISTS DemoTable (name, score)');
+        tx.executeSql('INSERT INTO DemoTable VALUES (?,?)', ['Alice', 101]);
+        tx.executeSql('INSERT INTO DemoTable VALUES (?,?)', ['Betty', 202]);
+      }, function(error) {
+        console.log('Transaction ERROR: ' + error.message);
+      }, function() {
+        console.log('Populated database OK');
+      });
+
+      db.transaction(function(tx) {
+        tx.executeSql('SELECT count(*) AS mycount FROM DemoTable', [], function(tx, rs) {
+          console.log('Record count (expected to be 2): ' + rs.rows.item(0).mycount);
+        }, function(tx, error) {
+          console.log('SELECT error: ' + error.message);
+        });
+      });
 
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
     document.getElementById('deviceready').classList.add('ready');
